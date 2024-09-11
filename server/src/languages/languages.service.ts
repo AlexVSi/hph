52644cf1789b8156e846eb61d6dccf72316863e7
AlexVSi <aleksandrsid2003@gmail.com>
2024-09-11 20:48:02 +0300

    create language service

--- server/src/languages/languages.service.ts
+++ server/src/languages/languages.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Language } from './languages.model';
+import { AddLanguageDto } from './dto/add-language.dto';
+import { UpdateLanguageDto } from './dto/update-language.dto';
+import { DeleteLanguageDto } from './dto/delete-language.dto';
 
 @Injectable()
-export class LanguagesService {}
+export class LanguagesService {
+    constructor(@InjectModel(Language) private languageRepository: typeof Language) { }
+
+    async getLanguage() {
+        const languages = await this.languageRepository.findAll()
+        return languages
+    }
+
+    async addLanguage(dto: AddLanguageDto) {
+        const language = await this.languageRepository.create(dto)
+        return language
+    }
+
+    async updateLanguage(dto: UpdateLanguageDto) {
+        const language = await this.languageRepository.findByPk(dto.languageId)
+        language.update(dto)
+        await language.save()
+        return language
+    }
+
+    async deleteLanguage(dto: DeleteLanguageDto) {
+        await this.languageRepository.destroy({ where: { id: dto.languageId } })
+    }
+}
