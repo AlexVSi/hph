8ccaef68ee7903b2628e2cbc7dcbfc1d942b3201
AlexVSi <aleksandrsid2003@gmail.com>
2024-09-19 13:17:43 +0300

    create language service

--- server/src/languages/languages.module.ts
+++ server/src/languages/languages.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { LanguagesService } from './languages.service';
 import { LanguagesController } from './languages.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Language } from './languages.model';
 
 @Module({
   providers: [LanguagesService],
-  controllers: [LanguagesController]
+  controllers: [LanguagesController],
+  imports: [
+    SequelizeModule.forFeature([Language])
+  ]
 })
 export class LanguagesModule {}
